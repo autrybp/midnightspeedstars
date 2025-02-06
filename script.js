a9723b3879374a1b5fdcc38abe678521a0cbd73b
Autry <111505727+autrybp@users.noreply.github.com>
2025-02-06 04:34:39 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("changeColor").addEventListener("click", function() {
+    document.body.style.backgroundColor = document.body.style.backgroundColor === "white" ? "black" : "white";
+    document.body.style.color = document.body.style.color === "red" ? "white" : "red";
+});
