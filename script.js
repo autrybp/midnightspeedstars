99f61b44a6e3ee0c7ba8c4b9b75145b1468ef7b0
Autry <111505727+autrybp@users.noreply.github.com>
2025-02-08 01:15:41 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("changeColor").addEventListener("click", function() {
-    document.body.style.backgroundColor = document.body.style.backgroundColor === "white" ? "black" : "white";
-    document.body.style.color = document.body.style.color === "red" ? "white" : "red";
-});
